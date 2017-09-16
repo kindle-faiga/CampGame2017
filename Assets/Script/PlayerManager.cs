955f2940bd937edbb1f3c13140565909022403e8
kindle-faiga <ts.gold.fire@gmail.com>
2017-09-16 19:31:09 +0900

    [add] プレイヤーの作成

---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour 
+{
+    [SerializeField]
+    private bool inverse;
+    [SerializeField]
+    private float jumpForce = 100.0f;
+	[SerializeField]
+	private float speed = 10.0f;
+    [SerializeField]
+    private KeyCode jumpKey;
+    private Rigidbody2D rb;
+    private bool isJump = false;
+
+	private void Start () 
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	private void Update ()
+    {
+        if(!isJump)
+        {
+            if (Input.GetKeyDown(jumpKey))
+            {
+                isJump = true;
+            }
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if(isJump)
+        {
+            rb.AddForce(new Vector2(0, inverse ? -jumpForce : jumpForce));
+            isJump = false;
+        }
+
+        if (Input.GetKey(jumpKey))
+		{
+            transform.position += new Vector3(speed, 0, 0);
+		}
+    }
+}
