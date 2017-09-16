93e1cc4eb48e481c47258542485596cb3e2aec14
kindle-faiga <ts.gold.fire@gmail.com>
2017-09-17 01:27:32 +0900

    [add] 障害物 リスポーン機能

---
+++ Assets/Script/DeadManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadManager : MonoBehaviour 
+{
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+        if(other.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
