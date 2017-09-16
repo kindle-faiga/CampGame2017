213ac533a2ee57c3a0a0f99d0f6d6c38c28b6acb
kindle-faiga <ts.gold.fire@gmail.com>
2017-09-17 00:58:33 +0900

    [add] タップ判定追加

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -2,32 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour 
+public class PlayerManager : MonoBehaviour
 {
     [SerializeField]
     private bool inverse;
     [SerializeField]
     private float jumpForce = 100.0f;
-	[SerializeField]
-	private float speed = 10.0f;
+    [SerializeField]
+    private float speed = 10.0f;
     [SerializeField]
     private KeyCode jumpKey;
-    private Rigidbody2D rb;
-    private bool isJump = false;
 
-	private void Start () 
+    private Rigidbody2D rb;
+    private GameObject tapObject;
+    private float depth = 10.0f;
+    private bool isJump = false;
+    private bool isGround = true;
+    private bool isReleased = false;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	private void Update ()
-    {
-        if(!isJump)
+        tapObject = GameObject.Find("Tap_Fields/" + transform.name);
+    }
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+        if (col.gameObject.tag.Equals("Player"))
         {
+            isGround = true;
+            isReleased = false;
+        }
+	}
+
+    private bool GetTouchAction(TouchPhase phase)
+    {
+		if (0 < Input.touchCount)
+		{
+			for (int i = 0; i < Input.touchCount; i++)
+			{
+				Touch t = Input.GetTouch(i);
+
+                if (t.phase == phase)
+				{
+					RaycastHit2D hit = IsSelected(t.position);
+
+					if (hit && hit.collider.gameObject.Equals(tapObject))
+					{
+                        return true;
+					}
+				}
+			}
+		}
+
+        return false;
+    }
+
+    private RaycastHit2D IsSelected(Vector3 position)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        return Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, depth, 1 << LayerMask.NameToLayer("Tap"));
+    }
+
+    private void Update()
+    {
+        if (!isJump && isGround)
+        {
+#if UNITY_EDITOR
             if (Input.GetKeyDown(jumpKey))
             {
                 isJump = true;
+                isGround = false;
             }
+#endif
+#if UNITY_ANDROID
+            if(GetTouchAction(TouchPhase.Began))
+            {
+                isJump = true;
+				isGround = false;
+			}
+#endif
+        }
+
+        if (!isGround)
+        {
+#if UNITY_EDITOR
+            if (Input.GetKeyUp(jumpKey))
+            {
+                isReleased = true;
+            }
+#endif
+#if UNITY_ANDROID
+            if (GetTouchAction(TouchPhase.Ended))
+            {
+                isReleased = true;
+            }
+#endif
         }
 	}
 
@@ -39,9 +109,17 @@ public class PlayerManager : MonoBehaviour
             isJump = false;
         }
 
-        if (Input.GetKey(jumpKey))
+#if UNITY_EDITOR
+        if (Input.GetKey(jumpKey) && !isReleased)
 		{
             transform.position += new Vector3(speed, 0, 0);
 		}
-    }
+#endif
+#if UNITY_ANDROID
+        if(GetTouchAction(TouchPhase.Stationary) && !isReleased)
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+#endif
+	}
 }
